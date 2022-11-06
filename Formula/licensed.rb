3efc453aadb1c353cac649900bc8fd9d46af2ad4
Felix Furrer <ffurrer2@users.noreply.github.com>
2022-11-06 22:15:10 +0100

    Add macOS ARM support

--- Formula/licensed.rb
+++ Formula/licensed.rb
@@ -5,8 +5,8 @@ class Licensed < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/github/licensed/releases/download/3.8.0/licensed-3.8.0-darwin-x64.tar.gz"
+    if Hardware::CPU.arm? || Hardware::CPU.intel?
+      url "https://github.com/github/licensed/releases/download/#{version}/licensed-#{version}-darwin-x64.tar.gz"
       sha256 "cddd6e277abab7dabfd56b93999b45fc70e4c7b06bd32619a506569552297f02"
 
       def install
@@ -17,7 +17,7 @@ class Licensed < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/github/licensed/releases/download/3.8.0/licensed-3.8.0-linux-x64.tar.gz"
+      url "https://github.com/github/licensed/releases/download/#{version}/licensed-#{version}-linux-x64.tar.gz"
       sha256 "9bb80cd587a2d2bf0c69c919e11f8261c74be5e9887e90b2fd561469940e1460"
 
       def install
