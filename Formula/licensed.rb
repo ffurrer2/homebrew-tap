bf32442f8829d9e9109a426231f3e714183bb8a1
Felix Furrer <ffurrer2@users.noreply.github.com>
2022-12-09 18:58:40 +0100

    Brew formula update for licensed version v3.9.1

--- Formula/licensed.rb
+++ Formula/licensed.rb
@@ -1,13 +1,13 @@
 class Licensed < Formula
   desc "A Ruby gem to cache and verify the licenses of dependencies"
   homepage "https://github.com/github/licensed"
-  version "3.9.0"
+  version "3.9.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm? || Hardware::CPU.intel?
       url "https://github.com/github/licensed/releases/download/#{version}/licensed-#{version}-darwin-x64.tar.gz"
-      sha256 "e981c9902e6213857d801b57d4c1fbaa268a84c062bf76b83569e3061805434a"
+      sha256 "fdf16ff6280351daa66896c6b7f477e8a057694be788c84e8b7dede02e57de0d"
 
       def install
         bin.install "licensed"
@@ -18,7 +18,7 @@ class Licensed < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/github/licensed/releases/download/#{version}/licensed-#{version}-linux-x64.tar.gz"
-      sha256 "abcf583591c820e24b7bf928017a2f51c5ba9c1b9000407aba3d5c71e2c98a5d"
+      sha256 "3a0bd5b8345a435397b38008d92bbb8b4ab1a673afec68a9f81399c9542ac7a8"
 
       def install
         bin.install "licensed"
